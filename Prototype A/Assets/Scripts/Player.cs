a28aa0623e573451a2eb3b4c114d3660587e0939
Alex Wittmann <alexwittmann01@gmail.com>
2019-09-08 19:07:58 -0600

    Start of Prototype
    Enemy follows player

---
+++ Prototype A/Assets/Scripts/Player.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    Rigidbody rb;
+    public float speed = 10;
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    void Update()
+    {
+        transform.Translate(Input.GetAxis("Vertical") * Time.deltaTime * speed, 0f, - Input.GetAxis("Horizontal")*Time.deltaTime*speed);
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("here1");
+        if (collision.gameObject.tag == "Wall")
+        {
+            Debug.Log("here2");
+            rb.velocity = Vector3.zero;
+            
+        }
+    }
+}
