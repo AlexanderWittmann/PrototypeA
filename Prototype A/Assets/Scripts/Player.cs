e355d0d3187bbc9132347f11d6e37f5800a3ab30
Alex <alexwittmann01@gmail.com>
2019-09-10 14:44:53 -0600

    Updated movement

--- Prototype A/Assets/Scripts/Player.cs
+++ Prototype A/Assets/Scripts/Player.cs
@@ -11,24 +11,26 @@ public class Player : MonoBehaviour
     {
         rb = GetComponent<Rigidbody>();
     }
-    void FixedUpdate()
+    void Update()
     {
-        //transform.Translate(Input.GetAxis("Vertical") * Time.deltaTime * speed, 0f, -Input.GetAxis("Horizontal") * Time.deltaTime * speed);
+        transform.Translate(Input.GetAxis("Vertical") * Time.deltaTime * speed, 0f, -Input.GetAxis("Horizontal") * Time.deltaTime * speed);
 
-        float moveHorizontal = Input.GetAxis("Horizontal");
-        float moveVertical = Input.GetAxis("Vertical");
+        //float moveHorizontal = Input.GetAxis("Horizontal");
+        //float moveVertical = Input.GetAxis("Vertical");
 
-        Vector3 movement = new Vector3(moveVertical, 0f, -moveHorizontal);
+        //Vector3 movement = new Vector3(moveVertical, 0f, -moveHorizontal);
 
-        rb.AddForce(movement * speed);
+        //rb.AddForce(movement * speed);
+        //transform.position += new Vector3(moveVertical, 0f, -moveHorizontal);
     }
 
     private void OnTriggerEnter(Collider other)
     {
         if(other.gameObject.tag == "Wall")
         {
-            Vector3 current = rb.velocity;
-            rb.velocity = -current/(7.5f);
+            rb.velocity = Vector3.zero;
+            //Vector3 current = rb.velocity;
+            //rb.velocity = -current/(7.5f);
         }
         if(other.gameObject.tag == "Enemy")
         {
